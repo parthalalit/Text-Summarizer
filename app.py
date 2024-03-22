c69d54dda9c8e102fb8201082bc4c820f90489d3
Partha Lalit <12parthlalit@gmail.com>
2024-03-22 20:42:30 +0530

    streamlit deployment

--- app.py
+++ app.py
@@ -1,45 +1,33 @@
-from fastapi import FastAPI
-import uvicorn
+#from fastapi import FastAPI
+#import uvicorn
 import sys
 import os
-from fastapi.templating import Jinja2Templates
-from starlette.responses import RedirectResponse
-from fastapi.responses import Response
+#from fastapi.templating import Jinja2Templates
+#from starlette.responses import RedirectResponse
+#from fastapi.responses import Response
+import streamlit as st 
 from textSummarizer.pipeline.prediction import PredictionPipeline
 
 
-text:str = "What is Text Summarization?"
+def main():
+    # Set title and description
+    st.title('Text Summarizer App')
+    st.write('This is an end-to-end text summarizer pipeline.')
 
-app = FastAPI()
+    # Load your model or initialize your pipeline
+    summarizer = PredictionPipeline()
 
-@app.get("/", tags=["authentication"])
-async def index():
-    return RedirectResponse(url="/docs")
+    # Add user input for text to be summarized
+    text_input = st.text_area('Enter text to summarize:', '')
 
+    # Add a button to trigger the summarization
+    if st.button('Summarize'):
+        # Perform summarization
+        summary = summarizer.summarize(text_input)
 
+        # Display the summary
+        st.subheader('Summary:')
+        st.write(summary)
 
-@app.get("/train")
-async def training():
-    try:
-        os.system("python main.py")
-        return Response("Training successful !!")
-
-    except Exception as e:
-        return Response(f"Error Occurred! {e}")
-    
-
-
-
-@app.post("/predict")
-async def predict_route(text):
-    try:
-
-        obj = PredictionPipeline()
-        text = obj.predict(text)
-        return text
-    except Exception as e:
-        raise e
-    
-
-if __name__=="__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8180) 
+if __name__ == '__main__':
+    main()
